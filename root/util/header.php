8c7548e24582ae69710cc2e686cca339c25e6f6c
L3tum <9307432+L3tum@users.noreply.github.com>
2019-10-01 19:39:59 +0200

    Move jquery

--- root/util/header.php
+++ root/util/header.php
@@ -10,6 +10,10 @@
             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
             crossOrigin="anonymous"
     />
+    <script
+            src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossOrigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet"/>
     <link rel="icon" type="image/x-icon" href="../favicon.ico"/>
     <link rel="stylesheet" href="../rabe.css"/>
